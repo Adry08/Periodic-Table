4f7521c04157464dca2f75de955a5d39d08987a5
Adry08 <112399639+Adry08@users.noreply.github.com>
2023-07-21 22:04:47 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -90,10 +90,10 @@ const click = (el) =>{
                 findEl(a,b)
             };
             
-        })
-    })
+        });
+    });
 
-}
+};
 
 const findEl = (a,b) =>{
     // console.log(a);
@@ -108,26 +108,41 @@ const findEl = (a,b) =>{
                 output(data);
 
             }i++;
-        }
+        };
 
-}
+};
+
+const remove = ()=>{
+
+    const remove = document.querySelectorAll('.remove');
+    remove.forEach((e)=>{
+        
+        e.addEventListener('click',()=>{
+    
+        table.childNodes.forEach((e)=>{e.style.cursor = "";})
+        table.classList.remove('disable');
+        container.style.display = 'none';
+        // table.style.color = 'black';
+        text.innerHTML = 'Select an element';
+    });
+    });
+};
+
+
+
 const container = document.getElementById('output');
 const output = (e)=>{
     const data = e;
     
     container.className = "output"
-    container.innerHTML = `<div id="remove">X</div><span id="number">${data.number}</span><span id="symbol">${data.symbol}</span><span id="atomic_mass">${data.atomic_mass.toLocaleString()}</span><button id="btnName">${data.name}</button>`;
+    container.innerHTML = `<div class="remove">X</div><span id="number">${data.number}</span><span id="symbol">${data.symbol}</span><span id="atomic_mass">${data.atomic_mass.toLocaleString()}</span><button id="btnName">${data.name}</button>`;
     container.style.display = 'grid';
 
-    const remove = document.getElementById('remove');
+    remove();
 
-    remove.addEventListener('click',()=>{
-        table.childNodes.forEach((e)=>{e.style.cursor = "";})
-        table.classList.remove('disable');
-        container.style.display = 'none';
-        table.style.color = 'black';
-        text.innerHTML = 'Select an element';
-    })
+   
+
+   
 
     const btnName = document.getElementById('btnName');
 
@@ -138,13 +153,47 @@ const output = (e)=>{
         const dataKeys = Object.keys(data);
         const dataValue = Object.values(data);
         
-       
+        const btnRemove = document.createElement('div');
+        btnRemove.className = 'remove';
+        btnRemove.innerText = 'X';
+        text.appendChild(btnRemove);
 
-        for (let i = 0 ; i < dataKeys.length; i++) {
+        
 
-            // text.innerHTML = 
-            text.innerHTML += `<span><span class = 'upper'> ${dataKeys[i]} : </span> ${dataValue[i]}</span> <br>`;
-        }
+        // for (let i = 0 ; i < dataKeys.length; i++) {
+            
+            
+        //     text.innerHTML = 
+        //     text.innerHTML += `<span><span class = 'upper'> ${dataKeys[i]} : </span> ${dataValue[i]}</span> <br>`;
+        // }
+        
+        text.innerHTML += `<div class='container'><h2 class = 'name'>${data.name}</h2> 
+        <div class="img"><img src="${data.image.url}" alt="${data.image.attribution}" height="200px" width="200px" >
+        </div><div class="legend">${data.image.title}</div>
+        <div class="summary">${data.summary}</div></div>
+        <div class="container"><div class="general">
+        <span>Symbol : ${data.symbol}</span>
+        <span>Block : ${data.block}</span>
+        <span>Atomic mass : ${data.atomic_mass}</span>
+        <span>Appearance : ${data.appearance}</span>
+        <span>Number : ${data.number}</span>
+        <span>Category : ${data.category}</span>
+        </div></div>
+        <div class="container"><div class="stat">
+        <span>Boil : ${data.boil}</span>
+        <span>Discovered by : ${data.discovered_by}</span>
+        <span>Density : ${data.density}</span>
+        <span>Melt : ${data.melt}</span>
+        <span>Period : ${data.period}</span>
+        <span>Group : ${data.group}</span>
+        <span>Phase : ${data.phase}</span>
+        <span>Electron affinity : ${data.electron_affinity}</span>
+        <span>Electronegativity pauling : ${data.electronegativity_pauling}</span>
+        </div></div>
+        <div class="container"><a href="${data.source}">More</a></div>
+        `
+
+        remove();
         
         
     })
@@ -156,3 +205,45 @@ getData(trieElement);
 // getData(click)
 
 
+
+const answers = ["Br O H", "Ancient Egypt"]
+
+
+const btn = document.querySelectorAll('.buttonquiz')
+const containerquiz = document.querySelectorAll('.containerquiz')
+
+
+let i = 0
+
+btn.forEach((e) => {
+
+    e.addEventListener('click', () => {
+        if (i < answers.length) {
+
+            let rep = document.getElementById(`quiz${i}`)
+            if (e.value === answers[i]) {
+
+                rep.innerText = e.value;
+                rep.classList.remove('false')
+                rep.classList.add('true');
+
+                setTimeout(() => {
+                    containerquiz[i + 1].classList.remove('disable')
+                    containerquiz[i].classList.add('disable');
+                    i++
+                }, 1000)
+
+            } else {
+
+                rep.innerText = e.value;
+                rep.classList.add('false');
+                setTimeout(() => {
+                    rep.innerText = "____";
+                }, 500)
+
+            };
+
+        };
+    });
+
+});
